a9ccf4ab85b4530b43df76367b5b5f3269de42d1
OnceMore2020 <guanhwang2011@gmail.com>
2015-03-03 20:02:55 +0800

    修改颜色配置为默认

--- POWERLINESHELL/themes/oncemore.py
+++ POWERLINESHELL/themes/oncemore.py
@@ -1,35 +1,51 @@
-class Color(DefaultColor):
-    USERNAME_FG = 15
-    USERNAME_BG = 4
-    USERNAME_ROOT_BG = 1
+class DefaultColor:
+    """
+    This class should have the default colors for every segment.
+    Please test every new segment with this theme first.
+    """
+    USERNAME_FG = 250
+    USERNAME_BG = 240
+    USERNAME_ROOT_BG = 124
 
-    HOSTNAME_FG = 15
-    HOSTNAME_BG = 10
+    HOSTNAME_FG = 250
+    HOSTNAME_BG = 238
 
     HOME_SPECIAL_DISPLAY = True
-    PATH_FG = 250
-    PATH_BG = 22
-    CWD_FG = 15
-    SEPARATOR_FG = 14
+    HOME_BG = 31  # blueish
+    HOME_FG = 15  # white
+    PATH_BG = 237  # dark grey
+    PATH_FG = 250  # light grey
+    CWD_FG = 254  # nearly-white grey
+    SEPARATOR_FG = 244
 
-    READONLY_BG = 1
-    READONLY_FG = 7
+    READONLY_BG = 124
+    READONLY_FG = 254
 
-    REPO_CLEAN_FG = 14
-    REPO_CLEAN_BG = 0
-    REPO_DIRTY_FG = 3
-    REPO_DIRTY_BG = 0
+    SSH_BG = 166 # medium orange
+    SSH_FG = 254
 
-    JOBS_FG = 4
-    JOBS_BG = 8
+    REPO_CLEAN_BG = 148  # a light green color
+    REPO_CLEAN_FG = 0  # black
+    REPO_DIRTY_BG = 161  # pink/red
+    REPO_DIRTY_FG = 15  # white
 
+    JOBS_FG = 39
+    JOBS_BG = 238
+
+    CMD_PASSED_BG = 236
     CMD_PASSED_FG = 15
-    CMD_PASSED_BG = 2
+    CMD_FAILED_BG = 161
     CMD_FAILED_FG = 15
-    CMD_FAILED_BG = 1
 
-    SVN_CHANGES_FG = REPO_DIRTY_FG
-    SVN_CHANGES_BG = REPO_DIRTY_BG
+    SVN_CHANGES_BG = 148
+    SVN_CHANGES_FG = 22  # dark green
 
-    VIRTUAL_ENV_BG = 15
-    VIRTUAL_ENV_FG = 2
+    VIRTUAL_ENV_BG = 35  # a mid-tone green
+    VIRTUAL_ENV_FG = 00
+
+class Color(DefaultColor):
+    """
+    This subclass is required when the user chooses to use 'default' theme.
+    Because the segments require a 'Color' class for every theme.
+    """
+    pass
